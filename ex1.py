2a8fed6b822366f6ebcb1885c8ec58826602af48
gidi <gidi@creditguard.co.il>
2021-06-14 19:04:03 +0300

    remove the comment

--- ex1.py
+++ ex1.py
@@ -1,12 +1,12 @@
 import base64
 import math
 from hashlib import sha256
-# from cryptography.exceptions import InvalidSignature
-# from cryptography.hazmat.primitives.asymmetric import rsa
-# from cryptography.hazmat.backends import default_backend
-# from cryptography.hazmat.primitives import serialization
-# from cryptography.hazmat.primitives import hashes
-# from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 from base64 import b64encode
 
 
